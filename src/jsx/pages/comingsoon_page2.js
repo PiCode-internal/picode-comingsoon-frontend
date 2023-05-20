d65c276f59b239d1db398224bdf6b12dd27bad9b
krishna rudani <krishna.rudani@picode.xyz>
2023-05-20 15:33:39 +0530

    change GIF and work on resonsive screen Images

--- src/jsx/pages/comingsoon_page2.js
+++ src/jsx/pages/comingsoon_page2.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../../css/comingsoon2.css"
 import logo from "../../icons/logo.svg"
-import diamond from '../../images/comingsoon2/94038-loading-atom.gif'
+import diamond from '../../images/comingsoon2/animation_500_lhugtvgi.gif'
 import doctor1 from "../../images/comingsoon2/4194.jpg"
 import pencil from "../../images/comingsoon2/5270 3.png"
 import tools from "../../images/comingsoon2/5270 7.png"
@@ -20,6 +20,8 @@ import idea from "../../images/comingsoon2/37556.jpg"
 import roller from "../../images/comingsoon2/bwink_edu_01_single_02 1.png";
 import men from "../../images/comingsoon2/v1064-10 1.png"
 import eye from "../../images/comingsoon2/5270 14.png"
+import { Formik, Form } from 'formik';
+import axios from 'axios';
 
 
 
@@ -30,6 +32,44 @@ const ComingsoonPage2 = () => {
   const showImage = screenWidth > 450;
   const showImage2 = screenWidth > 320;
 
+  const [userEmail, setUserEmail] = useState([])
+  const [submitted, setSubmitted] = useState(false);
+  const [alreadySubmit, setAlreadySubmit] = useState(false)
+
+  const handleSubscribeTwo = async (values, { setSubmitting }) => {
+    try {
+      let payload = {
+        email: values?.email
+      }
+      const { data } = await axios.post('https://picode-comingsoon-backend.vercel.app/api/email/send', payload);
+      let userEmail = []
+      let storeEmail = localStorage.getItem("email")
+      if (storeEmail === null || storeEmail === undefined || storeEmail.length === 0) {
+        userEmail = [values.email]
+        localStorage.setItem('email', JSON.stringify(userEmail));
+        setSubmitted(true)
+        setSubmitting(false);
+
+      } else {
+        if (storeEmail?.includes(values.email)) {
+          setAlreadySubmit(true)
+          setSubmitting(false);
+        } else {
+          userEmail = JSON.parse(storeEmail);
+          console.log("userEmail", userEmail)// Parse the stored string back to an array
+          userEmail.push(values.email)
+          localStorage.setItem('email', JSON.stringify(userEmail));
+          setSubmitted(true)
+          setSubmitting(false);
+
+        }
+      }
+
+
+    } catch (error) {
+      console.error('Subscription error:', error);
+    }
+  };
 
 
   return (
@@ -98,6 +138,7 @@ const ComingsoonPage2 = () => {
           </>
           : null
       }
+
 
       <div className='doctor_div'>
         <img className='doctor1' src={doctor1} alt="doctor1" />
@@ -170,11 +211,75 @@ const ComingsoonPage2 = () => {
                 </h4>
               </div>
               <p className='extraText2'>Be the first to know.</p>
-              <div>
-                {/* <input className='email_input2' name="email" placeholder={"Enter you email address"} /> */}
-                <button className='btn email_btn2'>Subscribe</button>
+              <Formik
+                initialValues={{ email: "" }}
+                validate={values => {
+                  const errors = {};
+                  if (!values.email) {
+                    errors.email = 'Please enter the email address !';
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                  ) {
+                    errors.email = 'Invalid email address';
+                  }
+                  return errors;
+                }}
+                onSubmit={handleSubscribeTwo}
+              >
+                {({ values, handleChange, errors, touched, handleSubmit, isSubmitting }) => {
+                  return (
+                    <Form>
+                      {
+                        submitted ?
+                          <>
+                            <div className='subscribed2'>
+                              <p className='text-success thanks_text2'>Thanks for subscribing. We will update you!</p>
+                              <p>
+                                <a href="/" className='register_btn2' style={{ color: 'black' }} >
+                                  Register another!
+                                </a>
+                              </p>
+                            </div>
 
-              </div>
+
+                          </> : <>
+                            <div className='email_form2'>
+                              <div className='email_div2' >
+                                <input
+                                  className='email_input2'
+                                  name="email"
+                                  placeholder={"Enter you email address"}
+                                  value={values?.email}
+                                  onChange={handleChange}
+                                />
+                                {/* <p className='text-danger mt-2'> {errors.email && touched.email && errors.email}</p> */}
+                              </div>
+                              <div>
+                                {
+                                  alreadySubmit ?
+                                    <div className='subscribed2'>
+                                      <p>
+                                        <a href="/" className='register_btn2 mx-3' style={{ color: 'black' }} >
+                                          Register another!
+                                        </a>
+                                      </p>
+                                    </div>
+                                    : <button className='btn email_btn2' type='submit' disabled={isSubmitting}>Subscribe</button>
+                                }
+                              </div>
+
+                            </div>
+                            <div className='last_msg'>
+                              {alreadySubmit ? <div className='subscriber_msg2'> <p className='text-success thanks_text2'>Thanks for the interest, you are already in our subscriber list. We will keep you posted first on our updates</p> </div> : ""}
+                            </div>
+
+                          </>
+                      }
+                    </Form>
+                  )
+                }}
+              </Formik>
+
             </div>
           </div>
         </div>
