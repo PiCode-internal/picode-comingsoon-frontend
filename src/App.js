29ec6c0ce54ae00ded581b77656a966b23538549
krishna rudani <krishna.rudani@picode.xyz>
2023-06-03 17:51:23 +0530

    set logo and app name

--- src/App.js
+++ src/App.js
@@ -11,16 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" Component={() => {
-            if (email) {
-              return <ComingsoonPage2 />
-            } else {
-              return <ComingsoonPage />
-            }
-          }} exact /> */}
           <Route path="/" Component={ComingsoonPage} exact />
-          {/* <Route path="/commingsoon2" Component={ComingsoonPage2} exact /> */}
-
         </Routes>
       </BrowserRouter>
     </div>
