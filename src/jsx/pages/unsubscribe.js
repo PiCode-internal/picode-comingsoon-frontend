14cedc48158c3643edd451b8be4909f183ba33a3
krishna rudani <krishna.rudani@picode.xyz>
2023-06-16 12:43:55 +0530

    design unsubscribe page

--- src/jsx/pages/unsubscribe.js
+++ src/jsx/pages/unsubscribe.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import "../../css/unsubscribe.css"
 import logo from "../../icons/logo.svg"
 import axios from 'axios';
+import unsubscribeImage from "../../images/download.jpeg"
 
 
 
@@ -20,18 +21,28 @@ const Unsubscribe = () => {
 
 
   return (
-    <div className='main_unsubscribe_div'>
+    <div className='main_wrapper'>
 
       <div className='container'>
         <div style={{ textAlign: "left" }}>
           <img className='logo1' src={logo} alt="logo" />
         </div>
-        <div className='row'>
-          <div className='col-12'>
-            <div className='unsubscribe_div'>
-              <h2 className='unsubscribe_title'>Picode Team</h2>
-              <p className='unsubscribe_text'>You’ve unsubscribed from our mailing list.</p>
-              <p className='unsubscribe_text'>You won’t receive any updates from us. You can subscribe again at any time</p>
+        <div className='main_unsubscribe_div'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='unsubscribe_div'>
+                <div>
+                  <img src={unsubscribeImage} className='unsub_img' alt="unsubscribe" />
+                </div>
+                <div>
+                  <h1 className='unsubscribe_title'>Thank you</h1>
+                  <p className='unsubscribe_text desktop'>You have been successfully removed from this subscriber list and <br /> won't recieve any future emails from us.</p>
+                  <p className='unsubscribe_text mobile'>You have been successfully removed from this subscriber list and  won't recieve any future emails from us.</p>
+                  <div className='resubscribe_div'>
+                    <p className='resubscribe_text'>Did you unsubscribe bt accident?</p> <a href='/' className='link_sub'>Re-subscribe</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
